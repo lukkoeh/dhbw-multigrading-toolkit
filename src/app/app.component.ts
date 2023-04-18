d6c627a54f122e37f63e294cab348ba8069f45ba
tdoerr <tim.doerr.22@lehre.mosbach.dhbw.de>
2023-04-18 20:00:28 +0200

    CSV Import mit SheetJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {read, readFile, utils} from "xlsx";
+import * as electron from "electron";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dhbw-multigrading-toolkit';
+  csvData = 'kacke';
+  async handleFile(event: Event){
+    const file:File = (event.currentTarget as HTMLInputElement).files![0];
+    const data = await file.arrayBuffer();
+    const workbook = read(data);
+    var jsonString = utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+    this.csvData = JSON.stringify(jsonString);
+  }
 }
+
+
