e5de14deadfd07ac72ce8133af93459a610a017d
Lukas Köhler <lukas.koehler@ueberbit.de>
2023-04-20 15:27:31 +0200

    upload testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,17 @@ import * as electron from "electron";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dhbw-multigrading-toolkit';
-  csvData = 'kacke';
+  title :string = 'dhbw-multigrading-toolkit';
+  csvData:string = 'kacke';
   async handleFile(event: Event){
     const file:File = (event.currentTarget as HTMLInputElement).files![0];
     const data = await file.arrayBuffer();
-    const workbook = read(data);
-    var jsonString = utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
-    this.csvData = JSON.stringify(jsonString);
+    const workbook = read(data, {sheetRows: 7});
+    var jsonObject : any = utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], {
+      header: 1,
+      raw: false,
+    });
+    this.csvData = JSON.stringify(jsonObject);
   }
 }
 
