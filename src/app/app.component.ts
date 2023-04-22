358b6565c76f65b99a34bd2408874b289804f363
Lukas Köhler <lukas.koehler@ueberbit.de>
2023-04-22 09:28:51 +0200

    Create logic to input html into view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import {Component, ViewChild} from '@angular/core';
-import {read, readFile, utils} from "xlsx";
-import * as electron from "electron";
+import {Component} from '@angular/core';
+import {read, utils} from "xlsx";
 
 
 @Component({
@@ -10,16 +9,19 @@ import * as electron from "electron";
 })
 export class AppComponent {
   title :string = 'dhbw-multigrading-toolkit';
-  csvData:string = 'kacke';
+  datastart : number = 10;
+  csvData : string = "<h1>bruh</h1>"
   async handleFile(event: Event){
     const file:File = (event.currentTarget as HTMLInputElement).files![0];
     const data = await file.arrayBuffer();
-    const workbook = read(data, {sheetRows: 7});
+    const workbook = read(data, /*{sheetRows: 7}*/);
     var jsonObject : any = utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], {
       header: 1,
       raw: false,
     });
-    this.csvData = JSON.stringify(jsonObject);
+    let dataarray = jsonObject.slice(this.datastart-1)
+    let metaarray = jsonObject.splice(0, 9)
+    this.csvData="<h1>blyat</h1>"
   }
 }
 
