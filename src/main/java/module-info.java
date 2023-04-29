d088e3b674c8cc3d1562117ca25cd4573254437d
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-04-29 09:31:08 +0200

    Add java.desktop in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module org.grp8.dhbwmultigradingtoolkit {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.desktop;
 
     opens org.grp8.dhbwmultigradingtoolkit to javafx.fxml;
     exports org.grp8.dhbwmultigradingtoolkit;
