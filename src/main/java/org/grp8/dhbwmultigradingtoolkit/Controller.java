db8d892119d0ff90463fc111fa258e4ac7207572
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-04-29 17:46:35 +0200

    Fix: Add @FXML annotation before each Button declartion
    Reason: The @FXML annotation is used in JavaFX to mark fields or methods that are defined in an XML file and that need to be injected with a reference to the corresponding JavaFX node when the XML file is loaded.

--- src/main/java/org/grp8/dhbwmultigradingtoolkit/Controller.java
+++ src/main/java/org/grp8/dhbwmultigradingtoolkit/Controller.java
@@ -9,17 +9,22 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class Controller {
-    //all button functions
+    //Declaration of Button
 
     @FXML
     private Button instruction;
+    @FXML
     private Button previewMatrikel;
+    @FXML
     private Button previewExam;
+    @FXML
     private Button exit;
+    @FXML
     private Button infoMatrikel;
+    @FXML
     private Button infoExam;
 
-    //linking "Anleitung"-Button t0 browser-pdf-document view
+    //linking Instruction-Icon to browser-pdf-document view
     @FXML
     private void openPdf(ActionEvent event) throws URISyntaxException, IOException {
         Desktop.getDesktop().browse(new URI("https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:f8ff6011-19f7-45aa-bdbe-ba5d5b228860"));
