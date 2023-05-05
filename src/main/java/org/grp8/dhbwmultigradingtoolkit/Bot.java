8a18904a46d22cb64e1fbfdaa14ba7ef91e44b4f
tdoerr <tim.doerr.22@lehre.mosbach.dhbw.de>
2023-05-05 18:34:50 +0200

    finish grade communication, restructure matriculation index & data coming from parsers

--- src/main/java/org/grp8/dhbwmultigradingtoolkit/Bot.java
+++ src/main/java/org/grp8/dhbwmultigradingtoolkit/Bot.java
@@ -1,29 +1,22 @@
 package org.grp8.dhbwmultigradingtoolkit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.*;
 
 
 import java.time.Duration;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
-import static java.util.concurrent.TimeUnit.*;
 
 
 public class Bot {
     private String starturl;
-    private WebDriver botwindow;
+    private final WebDriver botwindow;
     private String[] credentials = new String[2];
-    private HashMap<String, String> metainformation;
-    private ArrayList<ArrayList<String>> dataarray;
+    private final HashMap<String, String> metainformation;
+    private final ArrayList<ArrayList<String>> dataarray;
     public Bot(String[] credentials, SheetManager s) {
         this.metainformation = s.getMeta();
         this.dataarray = s.getData();
@@ -45,6 +38,33 @@ public class Bot {
             }
             if (Objects.equals(botwindow.getCurrentUrl(), this.starturl)) {
                 prepareGrading();
+                // get data (DOM elements, Sheet data) of all students in moodle table
+                List<WebElement> nodelist = botwindow.findElements(new By.ByClassName("unselectedrow"));
+                for (WebElement currentTableRow : nodelist) {
+                    // Zeile im Sheet (CSV, XLSX, ODS) basierend auf Namen in der Tabelle (Moodle) holen
+                    String rawName = currentTableRow.findElement(new By.ByClassName("c2")).getText();
+                    String formattedName = getFormattedStudentName(rawName);
+                    ArrayList<String> row = getRowByName(formattedName);
+
+                    // Bewertung (Prozentpunkte) eintragen
+                    WebElement gradeInputCell = currentTableRow.findElement(new By.ByClassName("c4"));
+                    WebElement gradeInputField = gradeInputCell.findElement(By.xpath(".//input"));
+
+                    gradeInputField.clear();
+                    // TODO: Evtl. Prozentpunkte errechnen, aktuell wird die Prozentpunkte-Spalte aus der Tabellenvorlage verwendet
+                    gradeInputField.sendKeys(row.get(4));
+
+
+                    // Feedback eintragen
+                    WebElement feedbackInputCell = currentTableRow.findElement(new By.ByClassName("c9"));
+                    WebElement feedbackInputField = feedbackInputCell.findElement(By.xpath(".//textarea"));
+                    feedbackInputField.clear();
+                    feedbackInputField.sendKeys("<p>" + row.get(5) + "</p>");
+
+                }
+                // Änderungen speichern
+                WebElement btnSaveGrades = botwindow.findElement(new By.ById("id_savequickgrades"));
+                btnSaveGrades.click();
 
             }
         }
@@ -63,22 +83,20 @@ public class Bot {
         return Objects.equals(botwindow.getCurrentUrl(), this.starturl);
     }
 
-    private String getCurrentName() {
-        WebElement w = new WebDriverWait(botwindow, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(new By.ByCssSelector("div[data-region = \"user-info\"] > h4 > a")));
-        String[] lines = w.getText().split("\\r?\\n");
-        String[] name = lines[0].split(" ");
+    private String getFormattedStudentName(String unformattedName) {
+        String[] name = unformattedName.split(" ");
         ArrayList<String> names = new ArrayList<>(Arrays.asList(name));
         names.remove(0);
         return String.join(" ", names);
     }
     public ArrayList<String> getRowByName(String name) {
-        for (int i = 0; i<this.dataarray.size(); i++) {
-            String tmp = String.join(" ", this.dataarray.get(i).get(1), this.dataarray.get(i).get(2));
+        for (ArrayList<String> strings : this.dataarray) {
+            String tmp = String.join(" ", strings.get(1), strings.get(2));
             if (Objects.equals(name, tmp)) {
-                return this.dataarray.get(i);
+                return strings;
             }
         }
-        throw new NoSuchElementException("Dataset was not found");
+        throw new NoSuchElementException("Dataset was not found: " + name);
     }
 
     public void insertGrade(String grade) {
@@ -103,7 +121,7 @@ public class Bot {
 
     }
 
-    public void clickSaveNext() throws InterruptedException {
+    public void clickSaveNext() {
         try {
             WebElement w = new WebDriverWait(botwindow, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(new By.ByCssSelector("button[name=\"saveandshownext\"]")));
             w.click();
@@ -116,18 +134,14 @@ public class Bot {
         }
     }
 
-    public void prepareGrading() throws InterruptedException {
-        WebElement w = new WebDriverWait(botwindow, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(new By.ById("id_quickgrading")));;
+    public void prepareGrading() {
+        WebElement w = new WebDriverWait(botwindow, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(new By.ById("id_quickgrading")));
         if (w.getAttribute("checked") == null) {
             w.click();
         }
         WebElement q = new WebDriverWait(botwindow, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(new By.ById("id_perpage")));
         Select se = new Select(q);
         se.selectByVisibleText("Alle");
-        List<WebElement> nodelist = botwindow.findElements(new By.ByClassName("unselectedrow"));
-        for (int i = 0; i< nodelist.size(); i++) {
-            String name = nodelist.get(i).findElement(new By.ByClassName("c2")).getText();
-            System.out.println(name);
-        }
+        // TODO: evtl. Teilnehmer benachrichtigen an
     }
 }
--- src/main/java/org/grp8/dhbwmultigradingtoolkit/HelloController.java
+++ src/main/java/org/grp8/dhbwmultigradingtoolkit/HelloController.java
@@ -2,18 +2,18 @@ package org.grp8.dhbwmultigradingtoolkit;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        SheetManager s = new SheetManager("C:\\Users\\lkoehler\\IdeaProjects\\dhbw-multigrading-toolkit\\parse.xlsx", "C:\\Users\\lkoehler\\IdeaProjects\\dhbw-multigrading-toolkit\\matrikel.xlsx");
+        SheetManager s = new SheetManager("C:\\Users\\derBoss\\Documents\\Sync\\dhbw\\Projektmanagement\\ProjektSemester2\\MoodleNotenTool\\dhbw-multigrading-toolkit\\parse.csv", "C:\\Users\\derBoss\\Documents\\Sync\\dhbw\\Projektmanagement\\ProjektSemester2\\MoodleNotenTool\\dhbw-multigrading-toolkit\\matrikel.xlsx");
         String[] creds = new String[2];
-        creds[0] = "luk.koehler.22@lehre.mosbach.dhbw.de";
-        creds[1] = "iPiQ1lm712H9t";
+        {
+            creds[0] = "tim.doerr.22@lehre.mosbach.dhbw.de";
+            creds[1] = "Neoeffects!22";
+        }
         Bot b = new Bot(creds, s);
         b.start();
     }
