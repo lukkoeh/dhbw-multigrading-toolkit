ac007bf9c6b76136fd35c71a5dd329bd34a3f771
Lukas Köhler <lukas.koehler@ueberbit.de>
2023-05-09 10:30:58 +0200

    Create input fields for moodle login data

--- src/main/java/org/grp8/dhbwmultigradingtoolkit/HelloController.java
+++ src/main/java/org/grp8/dhbwmultigradingtoolkit/HelloController.java
@@ -2,17 +2,25 @@ package org.grp8.dhbwmultigradingtoolkit;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import org.w3c.dom.Text;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private TextField txt_user;
+    @FXML
+    private TextField txt_password;
+
+    @FXML
     protected void onHelloButtonClick() {
-        SheetManager s = new SheetManager("C:\\Users\\derBoss\\Documents\\Sync\\dhbw\\Projektmanagement\\ProjektSemester2\\MoodleNotenTool\\dhbw-multigrading-toolkit\\parse.csv", "C:\\Users\\derBoss\\Documents\\Sync\\dhbw\\Projektmanagement\\ProjektSemester2\\MoodleNotenTool\\dhbw-multigrading-toolkit\\matrikel.xlsx");
+        SheetManager s = new SheetManager("C:\\Users\\lkoehler\\IdeaProjects\\dhbw-multigrading-toolkit\\parse.csv", "C:\\Users\\lkoehler\\IdeaProjects\\dhbw-multigrading-toolkit\\matrikel.xlsx");
         String[] creds = new String[2];
         {
-            creds[0] = "tim.doerr.22@lehre.mosbach.dhbw.de";
-            creds[1] = "denkste";
+            creds[0] = txt_user.getText();
+            creds[1] = txt_password.getText();
         }
         Bot b = new Bot(creds, s);
         b.start();
