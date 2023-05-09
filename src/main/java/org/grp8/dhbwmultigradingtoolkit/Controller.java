5d179cccf12088d1c828d253506a7e5e463fa05e
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-05-09 02:46:59 +0200

    Add functions to handle the file uploading - chooseFile: allows user to select the file - updateFileOutputLabel: to display the name of selected files + to show error prompt when no file is selected - updatePreviewButtonVisibility: to make Auge-Icon visible or invisible based on whether the file is uploaded or not - enableMoodleUploadButtonIfReady: disable "Moodle Upload" Button if either the files is null

--- src/main/java/org/grp8/dhbwmultigradingtoolkit/Controller.java
+++ src/main/java/org/grp8/dhbwmultigradingtoolkit/Controller.java
@@ -4,14 +4,19 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import java.awt.*;
 import java.io.IOException;
+import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
 import javafx.scene.input.MouseEvent;
 
 public class Controller {
@@ -25,7 +30,17 @@ public class Controller {
     @FXML
     private Button moodleUploadButton;
     @FXML
+    private Button fileUploadMatrikel;
+    @FXML
     private Button fileUploadGrade;
+    @FXML
+    private Label matrikelTabelleOutput;
+    @FXML
+    private Label notenTabelleOutput;
+
+    private File selectedMatrikelFile;
+    private File selectedGradeFile;
+
 
 
 
@@ -72,13 +87,111 @@ public class Controller {
         previewExamStage.showAndWait();
     }
 
-    //disable Moodle-Upload-Button
-    public final void disableButton(){
-        if (fileUploadGrade.getOnMousePressed()){
-            moodleUploadButton.setDisable(true);
-        }
-        else {
-            moodleUploadButton.setDisable(false);
+
+
+
+    /**
+     * Event handler for the file upload action of the Matrikeltabelle file.
+     * Prompts the user to select a Matrikel file using a file chooser dialog,
+     * updates the file output label, enables the Moodle upload button if ready,
+     * and updates the visibility of the preview button based on whether a file is selected.
+     */
+    @FXML
+    public void handleFileUploadMatrikel() {
+        selectedMatrikelFile = chooseFile("Select Matrikeltabelle", "Excel files", "*.xlsx", "*.xls", "*.csv");
+        updateFileOutputLabel(matrikelTabelleOutput, selectedMatrikelFile);
+        enableMoodleUploadButtonIfReady();
+        updatePreviewButtonVisibility(previewMatrikel, selectedMatrikelFile != null);
+    }
+
+
+
+
+    /**
+     * Event handler for the file upload action of the Notentabelle file.
+     * Prompts the user to select a grade file using a file chooser dialog,
+     * updates the file output label, enables the Moodle upload button if ready,
+     * and updates the visibility of the preview button based on whether a file is selected.
+     */
+    @FXML
+    public void handleFileUploadGrade() {
+        selectedGradeFile = chooseFile("Select Notentabelle", "Excel files", "*.xlsx", "*.xls", "*.csv");
+        updateFileOutputLabel(notenTabelleOutput, selectedGradeFile);
+        enableMoodleUploadButtonIfReady();
+        updatePreviewButtonVisibility(previewExam, selectedGradeFile != null);
+    }
+
+
+
+
+    /**
+     * Opens a file chooser dialog for the user to select a file.
+     *
+     * @param title       The title of the file chooser dialog.
+     * @param description The description of the file types to be displayed in the dialog.
+     * @param extensions  The file extensions allowed to be selected.
+     * @return The selected file, or null if no file was chosen.
+     */
+    private File chooseFile(String title, String description, String... extensions) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle(title);
+        fileChooser.getExtensionFilters().add(new ExtensionFilter(description, extensions));
+        return fileChooser.showOpenDialog(null);
+    }
+
+
+
+
+    /**
+     * Updates the label to display the name of the selected file, or a prompt to choose a file if no file is selected.
+     * Also updates the style class of the label to indicate an error if no file is selected.
+     *
+     * @param label The label to be updated.
+     * @param file  The selected file.
+     */
+    private void updateFileOutputLabel(Label label, File file) {
+        if (file != null) {
+            label.setText(file.getName());
+            label.getStyleClass().remove("error");
+        } else {
+            label.setText("Please choose a file !");
+            label.getStyleClass().add("error");
         }
     }
+
+
+
+
+    /**
+     * Sets the visibility of the preview button based on whether the file is uploaded or not.
+     *
+     * @param previewButton The preview button to be updated.
+     * @param isVisible     Indicating whether the preview button should be visible.
+     */
+    private void updatePreviewButtonVisibility(Button previewButton, boolean isVisible) {
+        previewButton.setVisible(isVisible);
+    }
+
+
+
+
+    /**
+     * Enables or disables the moodleUploadButton based on the availability of both the selectedMatrikelFile and selectedGradeFile.
+     * The moodleUploadButton is disabled if either of the files is null.
+     */
+    private void enableMoodleUploadButtonIfReady() {
+        moodleUploadButton.setDisable(selectedMatrikelFile == null || selectedGradeFile == null);
+    }
+
+
+
+    //disable Moodle-Upload-Button
+//    public final void disableButton(){
+//        if (fileUploadGrade.getOnMousePressed()){
+//            moodleUploadButton.setDisable(true);
+//        }
+//        else {
+//            moodleUploadButton.setDisable(false);
+//        }
+//    }
 }
