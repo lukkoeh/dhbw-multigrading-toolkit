e6800db5f12666cd107c24fd9972fc79672df3ea
Lukas Köhler <lukas.koehler@ueberbit.de>
2023-05-09 10:34:22 +0200

    Add Passwordfield

--- src/main/java/org/grp8/dhbwmultigradingtoolkit/HelloController.java
+++ src/main/java/org/grp8/dhbwmultigradingtoolkit/HelloController.java
@@ -1,18 +1,15 @@
 package org.grp8.dhbwmultigradingtoolkit;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import org.w3c.dom.Text;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
     private TextField txt_user;
     @FXML
-    private TextField txt_password;
+    private PasswordField txt_password;
 
     @FXML
     protected void onHelloButtonClick() {
